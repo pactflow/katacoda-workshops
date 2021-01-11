5ec9165b55e8f5c4070f70c6e66a5eb12a214fa2
Matt Fellows <matt.fellows@onegeek.com.au>
2021-01-11 16:28:52 +1100

    wip: try installing openjdk in step 1 in bg

--- pactflow-getting-started-java/step6-shell.sh
+++ pactflow-getting-started-java/step6-shell.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo "Changing into directory of the provider project: /root/example-provider-springboot"
+cd /root/example-provider-springboot
+
 echo "Configuring provider step to use JDK 11"
 export JAVA_HOME="/usr/java/jdk-11"
 export PATH="/usr/java/jdk-11/bin:${PATH}"
