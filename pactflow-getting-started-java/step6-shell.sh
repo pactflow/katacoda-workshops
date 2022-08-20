a071ee1fb5b1bfb77d35e3319938a01f99a52560
Yousaf Nabi <yousaf@pactflow.io>
2022-08-20 01:07:18 +0100

    chore: getting-started-java - set GIT_COMMIT/BRANCH

--- pactflow-getting-started-java/step6-shell.sh
+++ pactflow-getting-started-java/step6-shell.sh
@@ -6,3 +6,5 @@ cd /root/example-provider-springboot
 echo "Configuring provider step to use JDK 11"
 export JAVA_HOME="/usr/java/jdk-11"
 export PATH="/usr/java/jdk-11/bin:${PATH}"
+export GIT_COMMIT=$(git rev-parse --short HEAD)
+export GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
