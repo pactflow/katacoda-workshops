4ad0b8cdfcb670c1c3b05cc094e0229a550e2642
Yousaf Nabi <yousaf@pactflow.io>
2022-08-24 21:12:14 +0100

    fix: pact-js-v3-demo-e2e - correct root path

--- pact-js-v3-e2e/intro-start.sh
+++ pact-js-v3-e2e/intro-start.sh
@@ -5,7 +5,7 @@ echo "=> Downloading pact-js"
 git clone https://github.com/pact-foundation/pact-js
 
 echo "Changing into directory of the consumer project: /root/pact-js/pact-js/examples/v3/e2e"
-cd /root/pact-js/pact-js/examples/v3/e2e
+cd /root/pact-js/examples/v3/e2e
 npm i
 
 clear && echo "Welcome to the Pact-JS V3 Tutorial, all the dependencies are installed, and you should be good to go!"
