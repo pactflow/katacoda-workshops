ed3532f26a9af77c7bbff3d3c0f79742f3cf7607
Yousaf Nabi <yousaf@pactflow.io>
2022-08-24 21:37:38 +0100

    chore: pact-js-v3-demo-e2e - add instructions to run

--- pact-js-v3-e2e/intro-start.sh
+++ pact-js-v3-e2e/intro-start.sh
@@ -4,9 +4,11 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash 
 echo "=> Downloading pact-js"
 git clone https://github.com/pact-foundation/pact-js
 
-echo "Changing into directory of the consumer project: /root/pact-js/pact-js/examples/v3/e2e"
+echo "Changing into directory of the consumer project: /root/pact-js/examples/v3/e2e"
 cd /root/pact-js/examples/v3/e2e
 npm i
 
 export GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 clear && echo "Welcome to the Pact-JS V3 Tutorial, all the dependencies are installed, and you should be good to go!"
+echo "You are in /root/pact-js/examples/v3/e2e directory"
+echo "You can switch to the editor tab, to look in the code - press ctrl+p or cmd +p in the editor window, to search for a specific file in the pact-js/examples/v3/e2e directory"
