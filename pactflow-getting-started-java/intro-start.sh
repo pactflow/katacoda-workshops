f9158ef84fde2f6f47af14a5c154c2c7f01f7ad3
Nicholas Nogradi <nicholas.nogradi@SOML-P7RVJDY.attlocal.net>
2025-08-22 10:40:51 -0500

    chore(deps): update openjdk to 24 for example-consumer

--- pactflow-getting-started-java/intro-start.sh
+++ pactflow-getting-started-java/intro-start.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-echo "Installing OpenJDK 15 for consumer"
+echo "Installing OpenJDK 24 for consumer"
 mkdir -p /usr/java
 cd /usr/java
-wget -c https://download.java.net/java/GA/jdk15.0.2/0d1cfde4252546c6931946de8db48ee2/7/GPL/openjdk-15.0.2_linux-x64_bin.tar.gz
-tar -xf openjdk-15.0.2_linux-x64_bin.tar.gz
-echo 'export JAVA_HOME=/usr/java/jdk-15.0.2' >> ~/.bashrc
-echo "export PATH=/usr/java/jdk-15.0.2/bin:${PATH}" >> ~/.bashrc
+wget -c https://download.java.net/java/GA/jdk24.0.2/fdc5d0102fe0414db21410ad5834341f/12/GPL/openjdk-24.0.2_linux-x64_bin.tar.gz
+tar -xf openjdk-24.0.2_linux-x64_bin.tar.gz
+echo 'export JAVA_HOME=/usr/java/jdk-24.0.2' >> ~/.bashrc
+echo "export PATH=/usr/java/jdk-24.0.2/bin:${PATH}" >> ~/.bashrc
 source ~/.bashrc
 echo "Installing OpenJDK 11 for provider"
 mkdir -p /usr/java
