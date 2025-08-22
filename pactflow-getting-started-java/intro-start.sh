5a09ca4919588f2fa05f7b3dae2a2504e3732368
Nicholas Nogradi <nicholas.nogradi@SOML-P7RVJDY.attlocal.net>
2025-08-22 12:18:43 -0500

    chore(deps): update openjdk to 17 for example-consumer-java & example-provider-springboot

--- pactflow-getting-started-java/intro-start.sh
+++ pactflow-getting-started-java/intro-start.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-echo "Installing OpenJDK 24 for consumer"
+echo "Installing OpenJDK 17 for consumer"
 mkdir -p /usr/java
 cd /usr/java
-wget -c https://download.java.net/java/GA/jdk24.0.2/fdc5d0102fe0414db21410ad5834341f/12/GPL/openjdk-24.0.2_linux-x64_bin.tar.gz
-tar -xf openjdk-24.0.2_linux-x64_bin.tar.gz
-echo 'export JAVA_HOME=/usr/java/jdk-24.0.2' >> ~/.bashrc
-echo "export PATH=/usr/java/jdk-24.0.2/bin:${PATH}" >> ~/.bashrc
+wget -c https://download.java.net/java/GA/jdk17.0.2/dfd4a8d0985749f896bed50d7138ee7f/8/GPL/openjdk-17.0.2_linux-x64_bin.tar.gz
+tar -xf openjdk-17.0.2_linux-x64_bin.tar.gz
+echo 'export JAVA_HOME=/usr/java/jdk-17.0.2' >> ~/.bashrc
+echo "export PATH=/usr/java/jdk-17.0.2/bin:${PATH}" >> ~/.bashrc
 source ~/.bashrc
-echo "Installing OpenJDK 11 for provider"
+echo "Installing OpenJDK 17 for provider"
 mkdir -p /usr/java
 cd /usr/java
-wget -c https://download.java.net/java/ga/jdk11/openjdk-11_linux-x64_bin.tar.gz
-tar -xf openjdk-11_linux-x64_bin.tar.gz
+wget -c https://download.java.net/java/GA/jdk17.0.2/dfd4a8d0985749f896bed50d7138ee7f/8/GPL/openjdk-17.0.2_linux-x64_bin.tar.gz
+tar -xf openjdk-17.0.2_linux-x64_bin.tar.gz
 echo "Installing jq"
 apt --yes install jq && cd ~ && clear && echo "Welcome to the PactFlow Tutorial, all the dependencies are installed, and you should be good to go!"
